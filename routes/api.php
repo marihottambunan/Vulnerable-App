8b78c37be21d0321ce4d3dc0670de93f1172649a
marihot.tambunan <marihottambunan37@gmail.com>
2025-08-05 08:34:21 +0700

    Update lagii

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Employee;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/unprotected-employees', function () {
+    $employees = Employee::all();
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Data karyawan tanpa proteksi.',
+        'data' => $employees
+    ]);
 });
